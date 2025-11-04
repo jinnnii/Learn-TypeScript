f2bd69405e61e52322b0532e4d45a7d4ab144949
jinnnii <jinnnii@naver.com>
2025-11-04 12:39:42 +0900

    Add Chapter2.Practice

---
+++ 2-types-projects-quiz/calculator.ts
@@ -0,0 +1,26 @@
+/**
+ * Let's make a calculator
+ */
+type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
+function calculate(command: Command, x: number, y: number): number {
+    switch (command) {
+        case "add":
+            return x + y;
+        case "substract":
+            return x - y;
+        case "multiply":
+            return x * y;
+        case "divide":
+            return x / y;
+        case "remainder":
+            return x % y;
+        default:
+            throw new Error("Unknown Error");
+    }
+}
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
